4cf35ab125a052d2a43d6e800719948893e970d1
JustOff <Off.Just.Off@gmail.com>
2020-01-25 21:17:18 +0200

    Improve runtime environment detection using pre-parsing directives
    1. Make uBlock for firefox-legacy have `env_firefox` defined as `true`.
    2. Add an additional token `env_legacy` which is `true` only for uBlock for firefox-legacy.

--- src/js/storage.js
+++ src/js/storage.js
@@ -916,6 +916,7 @@
     [ 'env_chromium', 'chromium' ],
     [ 'env_edge', 'edge' ],
     [ 'env_firefox', 'firefox' ],
+    [ 'env_legacy', 'legacy' ],
     [ 'env_mobile', 'mobile' ],
     [ 'env_safari', 'safari' ],
     [ 'cap_html_filtering', 'html_filtering' ],
