cf4086e9559136aff8f3964998b2daa14655d6ed
JustOff <Off.Just.Off@gmail.com>
2020-07-24 13:16:33 +0300

    Force updating filters due to filter syntax converter changes

--- src/js/start.js
+++ src/js/start.js
@@ -139,7 +139,7 @@ var onVersionReady = function(lastVersion) {
     µb.assets.purge('ublock-resources');
 
     // Force updating all filters due to filter syntax converter update.
-    if ( vAPI.app.compareVersions(lastVersion, "1.16.4.22") <= 0 ) {
+    if ( vAPI.app.compareVersions(lastVersion, "1.16.4.23") <= 0 ) {
         µb.assets.purge(/./, 'public_suffix_list.dat');
     }
 
