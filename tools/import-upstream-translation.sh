0f46980d619f8b8c5dd684a5bdb922b9d761e0be
hawkeye116477 <hawkeye116477@gmail.com>
2020-10-08 17:57:19 +0200

    Second pass to re-design the dashboard
    The changes in this commit are mostly related
    to the "Filter lists" pane.
    
    An eye icon has been added, which purpose is
    to view the content of a list. Clicking on the
    name of the list will toggle the list's
    checkbox.
    
    Co-authored-by:  gorhill <585534+gorhill@users.noreply.github.com>

--- tools/import-upstream-translation.sh
+++ tools/import-upstream-translation.sh
@@ -11,7 +11,7 @@ TEMP=${MAIN_PATH}/src/_locales/temp.json
 for i in "$@"; do
     mapfile -t locales <  <(find "$MAIN_PATH"/src/_locales/* -type d -exec basename {} \;)
     for locale in "${locales[@]}"; do
-        jq -r "{$i: .$i}" "$(realpath "$MAIN_PATH"/../uBlock/src/_locales/"$locale"/messages.json)" > "$TEMP"
+        jq --arg i "$i" -r '{($i): .[$i]}' "$(realpath "$MAIN_PATH"/../uBlock/src/_locales/"$locale"/messages.json)" > "$TEMP"
         jq -s add "$(realpath "$MAIN_PATH"/src/_locales/"$locale"/messages.json)" "$TEMP" > "$TEMP".2
         mv "$TEMP".2 "$(realpath "$MAIN_PATH"/src/_locales/"$locale"/messages.json)"
         rm -rf "$TEMP"
