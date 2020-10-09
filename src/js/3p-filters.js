590d8f5b92ddee349feff5bdcac49a2dfa0b40d1
hawkeye116477 <hawkeye116477@gmail.com>
2020-10-09 12:36:01 +0200

    Fifth pass to re-design the dashboard
    Fine tuned previous work.
    
    Started the use of variable-based color values.
    
    Co-authored-by:  gorhill <585534+gorhill@users.noreply.github.com>

--- src/js/3p-filters.js
+++ src/js/3p-filters.js
@@ -392,22 +392,19 @@ const onFilteringSettingsChanged = function() {
 
 /******************************************************************************/
 
-const onRemoveExternalList = function() {
-    var liEntry = uDom(this).ancestors('[data-listkey]'),
-        listKey = liEntry.attr('data-listkey');
-    if ( listKey ) {
-        liEntry.toggleClass('toRemove');
-        renderWidgets();
-    }
+const onRemoveExternalList = function(ev) {
+    const liEntry = ev.target.closest('[data-listkey]');
+    if ( liEntry === null ) { return; }
+    liEntry.classList.toggle('toRemove');
+    renderWidgets();
 };
 
 /******************************************************************************/
 
-var onPurgeClicked = function() {
-    var button = uDom(this),
-        liEntry = button.ancestors('[data-listkey]'),
-        listKey = liEntry.attr('data-listkey');
-    if ( !listKey ) { return; }
+const onPurgeClicked = function(ev) {
+    const liEntry = ev.target.closest('[data-listkey]');
+    const listKey = liEntry.getAttribute('data-listkey') || '';
+    if ( listKey === '' ) { return; }
 
     messaging.send('dashboard', { what: 'purgeCache', assetKey: listKey });
 
@@ -416,10 +413,10 @@ var onPurgeClicked = function() {
     // https://github.com/gorhill/uBlock/issues/1733
     //   An external filter list must not be marked as obsolete, they will
     //   always be fetched anyways if there is no cached copy.
-    liEntry.addClass('obsolete');
-    liEntry.removeClass('cached');
+    liEntry.classList.add('obsolete');
+    liEntry.classList.remove('cached');
 
-    if ( liEntry.descendants('input').first().prop('checked') ) {
+    if ( liEntry.querySelector('input[type="checkbox"]').checked ) {
         renderWidgets();
     }
 };
@@ -695,6 +692,11 @@ uDom('#lists').on('change', '.listEntry input', onListsetChanged);
 uDom('#lists').on('click', '.listEntry .remove', onRemoveExternalList);
 uDom('#lists').on('click', 'span.cache', onPurgeClicked);
 uDom('#externalLists').on('input', onFilteringSettingsChanged);
+
+uDom('#lists').on('click', '.listEntry label *', ev => {
+    if ( ev.target.matches('a,input,.forinput') ) { return; }
+    ev.preventDefault();
+});
 
 /******************************************************************************/
 
