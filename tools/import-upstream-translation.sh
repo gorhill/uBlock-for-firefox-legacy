3a30d79551e385b00f4d92098288c11ff83284ed
JustOff <Off.Just.Off@gmail.com>
2020-10-27 20:59:30 +0200

    Make translation import script work in MozillaBuild environment

--- tools/import-upstream-translation.sh
+++ tools/import-upstream-translation.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This script assumes a linux environment
 #
 # Example: import-upstream-translation.sh popupTipNoScripting1 popupTipNoScripting2 settingsNoScriptingPrompt
 
-SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
-MAIN_PATH="$SCRIPT_PATH"/..
-TEMP=${MAIN_PATH}/src/_locales/temp.json
+SCRIPT_PATH="$(cd "$(dirname "$0")">/dev/null 2>&1; pwd -P)"
+SRC_PATH="$SCRIPT_PATH"/../../uBlock/src/_locales/
+DST_PATH="$SCRIPT_PATH"/../src/_locales/
+TEMP="$DST_PATH"temp.json
+
+locales=($(find "$DST_PATH"* -type d -exec basename {} \;))
 
 for i in "$@"; do
-    mapfile -t locales <  <(find "$MAIN_PATH"/src/_locales/* -type d -exec basename {} \;)
     for locale in "${locales[@]}"; do
-        jq --arg i "$i" -r '{($i): .[$i]}' "$(realpath "$MAIN_PATH"/../uBlock/src/_locales/"$locale"/messages.json)" > "$TEMP"
-        jq -s add "$(realpath "$MAIN_PATH"/src/_locales/"$locale"/messages.json)" "$TEMP" > "$TEMP".2
-        mv "$TEMP".2 "$(realpath "$MAIN_PATH"/src/_locales/"$locale"/messages.json)"
-        rm -rf "$TEMP"
+        jq --arg i "$i" -r '{($i): .[$i]}' "${SRC_PATH}${locale}/messages.json" > "$TEMP"
+        jq -s add "${DST_PATH}${locale}/messages.json" "$TEMP" > "$TEMP".2
+        mv "$TEMP".2 "${DST_PATH}${locale}/messages.json"
+        rm -f "$TEMP"
     done
 done
 
