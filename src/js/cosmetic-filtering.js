gorhill/uBlock-for-firefox-legacy

commit 4e91155a8a0b03b93f5c40359699acc6f12ce4cc

Improve compile-time error reporting in the logger.
This fixes #278.

Co-authored-by:  gorhill <585534+gorhill@users.noreply.github.com>

src/js/cosmetic-filtering.js
@@ -395,25 +395,6 @@ var FilterContainer = function() {
     this.reEscapeSequence = /\\([0-9A-Fa-f]+ |.)/g;
     this.reSimpleHighGeneric1 = /^[a-z]*\[[^[]+]$/;
     this.reHighMedium = /^\[href\^="https?:\/\/([^"]{8})[^"]*"\]$/;
-    this.reNeedHostname = new RegExp([
-        '^',
-        '(?:',
-            [
-            '.+?:has',
-            '.+?:has-text',
-            '.+?:if',
-            '.+?:if-not',
-            '.+?:matches-css(?:-before|-after)?',
-            '.*?:xpath',
-            '.+?:style',
-            '.+?:-abp-contains', // ABP-specific for `:has-text`
-            '.+?:-abp-has',      // ABP-specific for `:if`
-            '.+?:contains'       // Adguard-specific for `:has-text`
-            ].join('|'),
-        ')',
-        '\\(.+\\)',
-        '$'
-    ].join(''));
 
     this.selectorCache = new Map();
     this.selectorCachePruneDelay = 10 * 60 * 1000; // 10 minutes
@@ -633,68 +614,54 @@ FilterContainer.prototype.compileGenericSelector = function(parsed, writer) {
 FilterContainer.prototype.compileGenericHideSelector = function(parsed, writer) {
     var selector = parsed.suffix;
 
-    // For some selectors, it is mandatory to have a hostname or entity:
-    //   ##.foo:-abp-contains(...)
-    //   ##.foo:-abp-has(...)
-    //   ##.foo:contains(...)
-    //   ##.foo:has(...)
-    //   ##.foo:has-text(...)
-    //   ##.foo:if(...)
-    //   ##.foo:if-not(...)
-    //   ##.foo:matches-css(...)
-    //   ##.foo:matches-css-after(...)
-    //   ##.foo:matches-css-before(...)
-    //   ##:xpath(...)
-    //   ##.foo:style(...)
-    if ( this.reNeedHostname.test(selector) ) {
-        µb.logger.writeOne(
-            '',
-            'error',
-            'Cosmetic filtering – invalid generic filter: ##' + selector
-        );
-        return;
-    }
-
-    var type = selector.charCodeAt(0),
-        key;
+    const type = selector.charCodeAt(0);
+    let key;
 
     if ( type === 0x23 /* '#' */ ) {
         key = this.keyFromSelector(selector);
-        if ( key === undefined ) { return; }
         // Simple selector-based CSS rule: no need to test for whether the
         // selector is valid, the regex took care of this. Most generic
         // selector falls into that category.
+        // - ###ad-bigbox
         if ( key === selector ) {
-            writer.push([ 0 /* lg */, key.slice(1) ]);
+            writer.push([ 0, key.slice(1) ]);
             return;
         }
-        // Complex selector-based CSS rule.
-        if ( µb.staticExtFilteringEngine.compileSelector(selector) !== undefined ) {
-            writer.push([ 1 /* lg+ */, key.slice(1), selector ]);
-        }
-        return;
-    }
-
-    if ( type === 0x2E /* '.' */ ) {
+    } else if ( type === 0x2E /* '.' */ ) {
         key = this.keyFromSelector(selector);
-        if ( key === undefined ) { return; }
         // Simple selector-based CSS rule: no need to test for whether the
         // selector is valid, the regex took care of this. Most generic
         // selector falls into that category.
+        // - ##.ads-bigbox
         if ( key === selector ) {
-            writer.push([ 2 /* lg */, key.slice(1) ]);
+            writer.push([ 2, key.slice(1) ]);
             return;
         }
-        // Complex selector-based CSS rule.
-        if ( µb.staticExtFilteringEngine.compileSelector(selector) !== undefined ) {
-            writer.push([ 3 /* lg+ */, key.slice(1), selector ]);
-        }
-        return;
     }
 
     var compiled = µb.staticExtFilteringEngine.compileSelector(selector);
-    if ( compiled === undefined ) { return; }
-    // TODO: Detect and error on procedural cosmetic filters.
+
+    // Invalid cosmetic filter, possible reasons:
+    // - Bad syntax
+    // - Procedural filters (can't be generic): the compiled version of
+    //   a procedural selector is NEVER equal to its raw version.
+    if ( compiled === undefined || compiled !== selector ) {
+        const who = writer.properties.get('assetKey') || '?';
+        µb.logger.writeOne('', 'error', `Invalid generic cosmetic filter in ${who} : ##${selector}`);
+        return;
+    }
+
+    // Complex selector-based CSS rule:
+    // - ###tads + div + .c
+    // - ##.rscontainer > .ellip
+    if ( key !== undefined ) {
+        writer.push([
+            type === 0x23 /* '#' */ ? 1 : 3,
+            key.slice(1),
+            selector ]
+        );
+        return;
+    }
 
     // https://github.com/gorhill/uBlock/issues/909
     //   Anything which contains a plain id/class selector can be classified
@@ -702,9 +669,8 @@ FilterContainer.prototype.compileGenericHideSelector = function(parsed, writer) 
     var matches = this.rePlainSelectorEx.exec(selector);
     if ( matches !== null ) {
         key = matches[1] || matches[2];
-        type = key.charCodeAt(0);
         writer.push([
-            type === 0x23 ? 1 : 3 /* lg+ */,
+            key.charCodeAt(0) === 0x23 /* '#' */ ? 1 : 3,
             key.slice(1),
             selector
         ]);
@@ -736,7 +702,11 @@ FilterContainer.prototype.compileGenericUnhideSelector = function(
 ) {
     // Procedural cosmetic filters are acceptable as generic exception filters.
     var compiled = µb.staticExtFilteringEngine.compileSelector(parsed.suffix);
-    if ( compiled === undefined ) { return; }
+    if ( compiled === undefined ) {
+        const who = writer.properties.get('assetKey') || '?';
+        µb.logger.writeOne('', 'error', `Invalid cosmetic filter in ${who} : ##${parsed.suffix}`);
+        return;
+    }
 
     // https://github.com/chrisaljoudi/uBlock/issues/497
     //   All generic exception filters are put in the same bucket: they are
@@ -759,7 +729,11 @@ FilterContainer.prototype.compileHostnameSelector = function(
     }
 
     var compiled = µb.staticExtFilteringEngine.compileSelector(parsed.suffix);
-    if ( compiled === undefined ) { return; }
+    if ( compiled === undefined ) {
+        const who = writer.properties.get('assetKey') || '?';
+        µb.logger.writeOne('', 'error', `Invalid cosmetic filter in ${who} : ##${parsed.suffix}`);
+        return;
+    }
 
     var domain = this.µburi.domainFromHostnameNoCache(hostname),
         hash;

src/js/static-ext-filtering.js
@@ -740,12 +740,6 @@
                 entryPoint.pseudoclass = compiled.pseudo;
             }
 
-            µb.logger.writeOne(
-                '',
-                'error',
-                'Cosmetic filtering – invalid filter: ' + raw
-            );
-
             return JSON.stringify(compiled);
         };
 

src/js/static-net-filtering.js
@@ -2161,7 +2161,8 @@ FilterContainer.prototype.compile = function(raw, writer) {
 
     // Ignore filters with unsupported options
     if ( parsed.unsupported ) {
-        µb.logger.writeOne('', 'error', 'Network filtering – invalid filter: ' + raw);
+        const who = writer.properties.get('assetKey') || '?';
+        µb.logger.writeOne('', 'error', `Invalid network filter in ${who}: ${raw}`);
         return false;
     }
 

src/js/storage.js
@@ -431,28 +431,33 @@
                 .replace('{{origin}}', options.origin);
     }
 
-    var µb = this;
-
-    var onSaved = function() {
-        var compiledFilters = µb.compileFilters(filters),
-            snfe = µb.staticNetFilteringEngine,
-            cfe = µb.cosmeticFilteringEngine,
-            acceptedCount = snfe.acceptedCount + cfe.acceptedCount,
-            discardedCount = snfe.discardedCount + cfe.discardedCount;
-        µb.applyCompiledFilters(compiledFilters, true);
-        var entry = µb.availableFilterLists[µb.userFiltersPath],
-            deltaEntryCount = snfe.acceptedCount + cfe.acceptedCount - acceptedCount,
-            deltaEntryUsedCount = deltaEntryCount - (snfe.discardedCount + cfe.discardedCount - discardedCount);
+    let onSaved = ( ) => {
+        const compiledFilters = this.compileFilters(
+            filters,
+            { assetKey: this.userFiltersPath }
+        );
+        const snfe = this.staticNetFilteringEngine;
+        const cfe = this.cosmeticFilteringEngine;
+        const acceptedCount = snfe.acceptedCount + cfe.acceptedCount;
+        const discardedCount = snfe.discardedCount + cfe.discardedCount;
+        this.applyCompiledFilters(compiledFilters, true);
+        const entry = this.availableFilterLists[this.userFiltersPath];
+        const deltaEntryCount =
+            snfe.acceptedCount +
+            cfe.acceptedCount - acceptedCount;
+        const deltaEntryUsedCount =
+            deltaEntryCount -
+            (snfe.discardedCount + cfe.discardedCount - discardedCount);
         entry.entryCount += deltaEntryCount;
         entry.entryUsedCount += deltaEntryUsedCount;
-        vAPI.storage.set({ 'availableFilterLists': µb.availableFilterLists });
-        µb.staticNetFilteringEngine.freeze();
-        µb.redirectEngine.freeze();
-        µb.staticExtFilteringEngine.freeze();
-        µb.selfieManager.destroy();
+        vAPI.storage.set({ 'availableFilterLists': this.availableFilterLists });
+        this.staticNetFilteringEngine.freeze();
+        this.redirectEngine.freeze();
+        this.staticExtFilteringEngine.freeze();
+        this.selfieManager.destroy();
     };
 
-    var onLoaded = function(details) {
+    let onLoaded = details => {
         if ( details.error ) { return; }
         // The comment, if any, will be applied if and only if it is different
         // from the last comment found in the user filter list.
@@ -469,7 +474,7 @@
         // If we reached this point, the filter quite probably needs to be
         // added for sure: do not try to be too smart, trying to avoid
         // duplicates at this point may lead to more issues.
-        µb.saveUserFilters(details.content.trim() + '\n' + filters, onSaved);
+        this.saveUserFilters(details.content.trim() + '\n' + filters, onSaved);
     };
 
     this.loadUserFilters(onLoaded);
@@ -739,7 +744,10 @@
 
     var onCompiledListLoaded2 = function(details) {
         if ( details.content === '' ) {
-            details.content = µb.compileFilters(rawContent);
+            details.content = µb.compileFilters(
+                rawContent,
+                { assetKey: assetKey }
+            );
             µb.assets.put(compiledPath, details.content);
         }
         rawContent = undefined;
@@ -818,19 +826,27 @@
 
 /******************************************************************************/
 
-µBlock.compileFilters = function(rawText) {
+µBlock.compileFilters = function(rawText, details) {
     let writer = new this.CompiledLineIO.Writer();
+
+    // Populate the writer with information potentially useful to the
+    // client compilers.
+    if ( details ) {
+        if ( details.assetKey ) {
+            writer.properties.set('assetKey', details.assetKey);
+        }
+    }
 
     // Useful references:
     //    https://adblockplus.org/en/filter-cheatsheet
     //    https://adblockplus.org/en/filters
-    let staticNetFilteringEngine = this.staticNetFilteringEngine,
-        staticExtFilteringEngine = this.staticExtFilteringEngine,
-        reIsWhitespaceChar = /\s/,
-        reMaybeLocalIp = /^[\d:f]/,
-        reIsLocalhostRedirect = /\s+(?:0\.0\.0\.0|broadcasthost|localhost|local|ip6-\w+)(?:[^\w.-]|$)/,
-        reLocalIp = /^(?:0\.0\.0\.0|127\.0\.0\.1|::1|fe80::1%lo0)/,
-        lineIter = new this.LineIterator(this.processDirectives(rawText));
+    let staticNetFilteringEngine = this.staticNetFilteringEngine;
+    let staticExtFilteringEngine = this.staticExtFilteringEngine;
+    let reIsWhitespaceChar = /\s/;
+    let reMaybeLocalIp = /^[\d:f]/;
+    let reIsLocalhostRedirect = /\s+(?:0\.0\.0\.0|broadcasthost|localhost|local|ip6-\w+)(?:[^\w.-]|$)/;
+    let reLocalIp = /^(?:0\.0\.0\.0|127\.0\.0\.1|::1|fe80::1%lo0)/;
+    let lineIter = new this.LineIterator(this.processDirectives(rawText));
 
     while ( lineIter.eot() === false ) {
         // rhill 2014-04-18: The trim is important here, as without it there
@@ -1300,7 +1316,10 @@
                     );
                     this.assets.put(
                         'compiled/' + details.assetKey,
-                        this.compileFilters(details.content)
+                        this.compileFilters(
+                            details.content,
+                            { assetKey: details.assetKey }
+                        )
                     );
                 }
             } else {

src/js/utils.js
@@ -234,8 +234,9 @@
         this.io = µBlock.CompiledLineIO;
         this.blockId = undefined;
         this.block = undefined;
-        this.blocks = new Map();
         this.stringifier = this.io.serialize;
+        this.blocks = new Map();
+        this.properties = new Map();
     },
 
     Reader: function(raw, blockId) {
@@ -246,6 +247,7 @@
         this.line = '';
         this.parser = this.io.unserialize;
         this.blocks = new Map();
+        this.properties = new Map();
         let reBlockStart = new RegExp(
             '^' + this.io.blockStartPrefix + '(\\d+)\\n',
             'gm'

commit aabe611de50233069ebbef74948ab701235fd1c9

Fix the handling of pseudoclass-based generic cosmetic filters
Related issues:
- uBlockOrigin/uBlock-issues#464
- gorhill/uBlock-for-firefox-legacy#290


Regression from:
4e91155a8a0b03b93f5c40359699acc6f12ce4cc

Pseudoclass-based generic cosmetic filters were improperly seen
as invalid following the regression.

Co-authored-by:  gorhill <585534+gorhill@users.noreply.github.com>

src/js/cosmetic-filtering.js
@@ -645,7 +645,14 @@ FilterContainer.prototype.compileGenericHideSelector = function(parsed, writer) 
     // - Bad syntax
     // - Procedural filters (can't be generic): the compiled version of
     //   a procedural selector is NEVER equal to its raw version.
-    if ( compiled === undefined || compiled !== selector ) {
+    // https://github.com/uBlockOrigin/uBlock-issues/issues/464
+    //   Pseudoclass-based selectors can be compiled, but are also valid
+    //   plain selectors.
+    if (
+        compiled === undefined ||
+        compiled !== selector &&
+        µb.staticExtFilteringEngine.compileSelector.pseudoclass === -1
+    ) {
         const who = writer.properties.get('assetKey') || '?';
         µb.logger.writeOne('', 'error', `Invalid generic cosmetic filter in ${who} : ##${selector}`);
         return;
@@ -933,7 +940,7 @@ FilterContainer.prototype.skipCompiledContent = function(reader) {
 FilterContainer.prototype.toSelfie = function() {
     var selfieFromMap = function(map) {
         var selfie = [];
-        // Note: destructuring assignment not supported before Chromium 49. 
+        // Note: destructuring assignment not supported before Chromium 49.
         for ( var entry of map ) {
             selfie.push([ entry[0], entry[1].compile() ]);
         }

src/js/static-ext-filtering.js
@@ -582,7 +582,7 @@
             return µb.cosmeticFilteringEngine.discardedCount +
                    µb.scriptletFilteringEngine.discardedCount +
                    µb.htmlFilteringEngine.discardedCount;
-        }
+        },
     };
 
     //--------------------------------------------------------------------------
@@ -703,6 +703,8 @@
         ]);
 
         const entryPoint = function(raw) {
+            entryPoint.pseudoclass = -1;
+
             const extendedSyntax = reExtendedSyntax.test(raw);
             if ( cssSelectorType(raw) === 1 && extendedSyntax === false ) {
                 return raw;
@@ -711,19 +713,17 @@
             // We  rarely reach this point -- majority of selectors are plain
             // CSS selectors.
 
-            let operator;
-
-            // Supported Adguard/ABP advanced selector syntax: will translate into
-            // uBO's syntax before further processing.
+            // Supported Adguard/ABP advanced selector syntax: will translate
+            // into uBO's syntax before further processing.
             // Mind unsupported advanced selector syntax, such as ABP's
             // `-abp-properties`.
-            // Note: extended selector syntax has been deprecated in ABP, in favor
-            // of the procedural one (i.e. `:operator(...)`). See
-            // https://issues.adblockplus.org/ticket/5287
+            // Note: extended selector syntax has been deprecated in ABP, in
+            // favor of the procedural one (i.e. `:operator(...)`).
+            // See https://issues.adblockplus.org/ticket/5287
             if ( extendedSyntax ) {
                 let matches;
                 while ( (matches = reExtendedSyntaxParser.exec(raw)) !== null ) {
-                    operator = normalizedExtendedSyntaxOperators.get(matches[1]);
+                    const operator = normalizedExtendedSyntaxOperators.get(matches[1]);
                     if ( operator === undefined ) { return; }
                     raw = raw.slice(0, matches.index) +
                           operator + '(' + matches[3] + ')' +
@@ -742,6 +742,8 @@
 
             return JSON.stringify(compiled);
         };
+
+        entryPoint.pseudoclass = -1;
 
         return entryPoint;
     })();